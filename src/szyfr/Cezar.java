4b8ef0ad39a76c09c43c5b975cd99f95bbbb5ce2
ulalu <lousalome23@gmail.com>
2019-04-25 14:47:47 +0200

    cezar

---
+++ src/szyfr/Cezar.java
@@ -0,0 +1,35 @@
+package szyfr;
+
+public class Cezar {
+
+	public static boolean isLetter(char znak) {
+		if (('A'<= znak && znak<='Z')||('a' <= znak && znak <='z'))
+			return true;
+		else
+			return false;
+	} 
+	
+	public static String Cezar(String tekst) {
+		String wyjsciowy="";
+		for(int i=0;i<tekst.length();i++)
+     {
+	if(isLetter(tekst.charAt(i)))
+			{
+		if (tekst.charAt(i)<91)
+		wyjsciowy+=((char)((tekst.charAt(i)-'A'+3))%26+'A');
+		else
+		wyjsciowy+=((char)(tekst.charAt(i)-'a'+3)%26+'a');
+	}
+   else {
+		wyjsciowy+=tekst.charAt(i);
+}
+     }
+return wyjsciowy;
+	}
+}
+}
+	
+
+
+
+//(znak - c)%25
