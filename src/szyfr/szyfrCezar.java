9ab767fed53b07f1b717059f24c8c22b024497c6
ulalu <lousalome23@gmail.com>
2019-05-11 20:22:21 +0200

    c

--- src/szyfr/szyfrCezar.java
+++ src/szyfr/szyfrCezar.java
@@ -64,11 +64,8 @@ public static void main(String[] args) throws FileNotFoundException{
 	
 	 String st= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 System.out.println(Cezar(st,3));
-<<<<<<< HEAD
-System.out.println(reverse_Cezar(Cezar(st,3)));
-=======
 System.out.println(reverseCezar(Cezar(st,5)));
->>>>>>> branch 'master' of https://github.com/louAphasia/cloudapp
+
 }
 }
 
